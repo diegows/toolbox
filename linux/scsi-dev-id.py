4de8d66d41967caf0029c3ba6324ad3588b428b0
Diego Woitasen <diego@woitasen.com.ar>
2012-01-27 00:03:24 -0300

    Check if the file is a block device.

--- linux/scsi-dev-id.py
+++ linux/scsi-dev-id.py
@@ -6,6 +6,8 @@
 #
 
 from subprocess import Popen, PIPE
+from os import stat
+from stat import S_ISBLK
 import sys
 
 if len(sys.argv) == 1:
@@ -17,6 +19,13 @@ def get_id(device):
     scsi_id = Popen(scsi_id_cmd.split(), stdout=PIPE)
     return scsi_id.stdout.readline().strip()
 
+def is_block(device):
+    dev_info = stat(device)
+    return S_ISBLK(dev_info.st_mode)
+
 for device in sys.argv[1:]:
+    if not is_block(device):
+        print "Device %s is not a block device." % (device)
+        sys.exit(-1)
     print device, get_id(device)
 
