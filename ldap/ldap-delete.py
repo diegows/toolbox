cfd57e57cac0a58ad2067e2aeea457901bfdeb11
Diego Woitasen <diego@woitasen.com.ar>
2013-01-26 14:47:38 -0300

    Use optparse to make it compatible with Python 2.6.

--- ldap/ldap-delete.py
+++ ldap/ldap-delete.py
@@ -7,19 +7,19 @@
 #
 
 import ldap
-import argparse
+from optparse import OptionParser
 
-parser = argparse.ArgumentParser(description='LDAP delete by filter.')
-parser.add_argument('--ldapuri', required=True)
-parser.add_argument('--ldapbinddn', required=True)
-parser.add_argument('--ldapbindpw', required=True)
-parser.add_argument('--searchbase', required=True)
-parser.add_argument('--filter', required=True)
-args = parser.parse_args()
+parser = OptionParser()
+parser.add_option('--ldapuri')
+parser.add_option('--ldapbinddn')
+parser.add_option('--ldapbindpw')
+parser.add_option('--searchbase')
+parser.add_option('--filter')
+(options, args) = parser.parse_args()
 
-new_ldap = ldap.initialize(args.ldapuri)
-new_ldap.bind_s(args.ldapbinddn, args.ldapbindpw)
-objs = new_ldap.search_s(args.searchbase, ldap.SCOPE_SUBTREE, args.filter)
+new_ldap = ldap.initialize(options.ldapuri)
+new_ldap.bind_s(options.ldapbinddn, options.ldapbindpw)
+objs = new_ldap.search_s(options.searchbase, ldap.SCOPE_SUBTREE, options.filter)
 
 for obj in objs:
     dn = obj[0]
